3dfbc7dba44403f9ac290b33e35b1aed32f07700
KLA <smtwkla@gmail.com>
2024-02-11 22:17:47 +0530

    working email reports

---
+++ send_email_rep.py
@@ -0,0 +1,14 @@
+import smtplib
+import email_secrets as es
+import email.message
+
+def send_mail(text):
+
+
+    mailserver = smtplib.SMTP(es.SMTP_SRV, es.SMTP_PORT)
+    mailserver.ehlo()
+    mailserver.starttls()
+    mailserver.login(es.SMTP_USER, es.SMTP_PASSWORD)
+    # Adding a newline before the body text fixes the missing message body
+    mailserver.sendmail(es.SMTP_USER, es.SMTP_SEND_TO, text)
+    mailserver.quit()
--- vault-backup.py
+++ vault-backup.py
@@ -6,12 +6,21 @@ import boto3
 import botocore.exceptions
 import logging
 
+import email_secrets
+import send_email_rep
+
 from secrets import *
 from settings import *
 
 
 ADMS_LOG_FILENAME = 'ADMSConsoleBackupLog.txt'
 VAULTBACKUP_LOG_FILENAME = 'vault-backup.log.txt'
+
+
+def setup_logging():
+    subprocess.run([r'cmd', r'/c', 'del', r'/Q', VAULTBACKUP_LOG_FILENAME])
+    logging.basicConfig(format='%(asctime)s %(message)s', filename=VAULTBACKUP_LOG_FILENAME, encoding='utf-8',
+                        level=logging.INFO)
 
 
 def upload_to_s3bucket(loc_file, s3_b, rem_file):
@@ -22,8 +31,20 @@ def upload_to_s3bucket(loc_file, s3_b, rem_file):
     s3.upload_file(loc_file, s3_b, rem_file)
 
 
-logging.basicConfig(format='%(asctime)s %(message)s', filename=VAULTBACKUP_LOG_FILENAME, encoding='utf-8',
-                    level=logging.INFO)
+def send_report_and_exit():
+    logging.getLogger().handlers[0].flush()
+
+    with open(VAULTBACKUP_LOG_FILENAME, 'r') as fl:
+        log_content = fl.read()
+
+    msg = f"""From: {email_secrets.SMTP_USER}
+    To: {email_secrets.SMTP_SEND_TO} 
+    Subject: Autodesk Vault Backup Report {datetime.datetime.now().strftime("%Y-%m-%d")}
+
+    {log_content}
+    """
+    send_email_rep.send_mail(msg)
+    exit(-1)
 
 
 adms = os.path.join(ADMSConsolePath, 'Connectivity.ADMSConsole.exe')
@@ -35,6 +56,8 @@ if len(sys.argv) > 1 and sys.argv[1] == "-d":
     bk_cmd = [os.path.join(wd, "dummybackup.bat")]
     ADMSConsolePath = os.path.join(wd)
 
+setup_logging()
+
 try:
     logging.info("Running ADMSConsole Backup...")
     c = subprocess.run(bk_cmd, cwd=ADMSConsolePath)
@@ -45,7 +68,7 @@ try:
     c = subprocess.run([r'tar', r'zcf', 'backups.tar.gz', 'Backups'], cwd=wd)
 except Exception as e:
     logging.exception(f"Error running {bk_cmd}.")
-    exit(-1)
+    send_report_and_exit()
 
 n = datetime.datetime.now().strftime("%Y_%m_%d_%H_")
 tar_rem_name = f'vault_backup_{n}.tar.gz'
@@ -56,13 +79,15 @@ log_rem_name = f'VaultBackupADMSLog.txt'
 try:
     upload_to_s3bucket(tar_loc_fullname, s3_bucket, tar_rem_name)
     upload_to_s3bucket(log_path, s3_bucket, log_rem_name)
+    pass
 except (WindowsError, botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
     logging.exception('Error occurred during S3 Upload.')
-    exit(-1)
+    send_report_and_exit()
 
 logging.info(f"Uploaded {log_rem_name} to S3.")
 subprocess.run([r'cmd', r'/c', 'rmdir', r'/S', r'/Q', os.path.join(wd, r'Backups')])
 subprocess.run([r'cmd', r'/c', 'del', r'/Q', tar_loc_fullname])
 subprocess.run([r'cmd', r'/c', 'del', r'/Q', log_path])
 logging.debug("Temp Files cleared.")
-logging.getLogger().handlers[0].flush()
+logging.debug("Success.")
+send_report_and_exit()
