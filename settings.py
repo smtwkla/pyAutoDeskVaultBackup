b6377dbe27c0ff4dd65e20e6792f4d3f130a9cbc
KLA <smtwkla@gmail.com>
2024-02-11 17:00:24 +0530

    logging added.

---
+++ settings.py
@@ -0,0 +1,2 @@
+wd = "c:\\backup\\vault"
+ADMSConsolePath = 'C:\\Program Files\\Autodesk\\Vault Server 2024\\ADMS Console'
--- vault-backup.py
+++ vault-backup.py
@@ -4,9 +4,14 @@ import subprocess
 import datetime
 import boto3
 import botocore.exceptions
-import traceback
+import logging
 
 from secrets import *
+from settings import *
+
+
+ADMS_LOG_FILENAME = 'ADMSConsoleBackupLog.txt'
+VAULTBACKUP_LOG_FILENAME = 'vault-backup.log.txt'
 
 
 def upload_to_s3bucket(loc_file, s3_b, rem_file):
@@ -17,39 +22,47 @@ def upload_to_s3bucket(loc_file, s3_b, rem_file):
     s3.upload_file(loc_file, s3_b, rem_file)
 
 
+logging.basicConfig(format='%(asctime)s %(message)s', filename=VAULTBACKUP_LOG_FILENAME, encoding='utf-8',
+                    level=logging.INFO)
+
+
 adms = os.path.join(ADMSConsolePath, 'Connectivity.ADMSConsole.exe')
-log_path = os.path.join(wd, "VaultBackupLog.txt")
+log_path = os.path.join(wd, ADMS_LOG_FILENAME)
 bk_cmd = [adms, fr'-Obackup', fr'-B{wd}\Backups', '-VUAdministrator', f'-VP{VPassword}', '-S',
           f'-L{log_path}']
 
-
 if len(sys.argv) > 1 and sys.argv[1] == "-d":
-    bk_cmd = os.path.join(wd, "dummybackup.bat")
+    bk_cmd = [os.path.join(wd, "dummybackup.bat")]
+    ADMSConsolePath = os.path.join(wd)
 
 try:
-    print("ADMSConsole Backup...")
+    logging.info("Running ADMSConsole Backup...")
     c = subprocess.run(bk_cmd, cwd=ADMSConsolePath)
-    print("Tar...")
+    logging.info("ADMSConsole Backup Complete. Logfile Contents:")
+    with open(log_path, "r") as f:
+        logging.info(f.read())
+    logging.info("Running tar...")
     c = subprocess.run([r'tar', r'zcf', 'backups.tar.gz', 'Backups'], cwd=wd)
 except Exception as e:
-    traceback.print_exc()
+    logging.exception(f"Error running {bk_cmd}.")
     exit(-1)
 
 n = datetime.datetime.now().strftime("%Y_%m_%d_%H_")
 tar_rem_name = f'vault_backup_{n}.tar.gz'
 tar_loc_fullname = os.path.join(wd, 'backups.tar.gz')
-log_rem_name = f'VaultBackupLog.txt'
-log_loc_fullname = os.path.join(wd, 'VaultBackupLog.txt')
+log_rem_name = f'VaultBackupADMSLog.txt'
+
 
 try:
     upload_to_s3bucket(tar_loc_fullname, s3_bucket, tar_rem_name)
-    upload_to_s3bucket(log_loc_fullname, s3_bucket, log_rem_name)
+    upload_to_s3bucket(log_path, s3_bucket, log_rem_name)
 except (WindowsError, botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
-    print('Error occurred.\n', e)
+    logging.exception('Error occurred during S3 Upload.')
     exit(-1)
 
-print("Uploaded to S3.")
+logging.info(f"Uploaded {log_rem_name} to S3.")
 subprocess.run([r'cmd', r'/c', 'rmdir', r'/S', r'/Q', os.path.join(wd, r'Backups')])
 subprocess.run([r'cmd', r'/c', 'del', r'/Q', tar_loc_fullname])
-subprocess.run([r'cmd', r'/c', 'del', r'/Q', log_loc_fullname])
-print("Temp Files cleared.")
+subprocess.run([r'cmd', r'/c', 'del', r'/Q', log_path])
+logging.debug("Temp Files cleared.")
+logging.getLogger().handlers[0].flush()
