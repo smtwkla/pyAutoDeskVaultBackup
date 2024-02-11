2cc59990d73bb651c7530f3390e85564e24b287d
KLA <smtwkla@gmail.com>
2024-02-11 10:40:40 +0530

    Initial commit

---
+++ vault-backup.py
@@ -0,0 +1,49 @@
+import os.path
+import sys
+import subprocess
+import datetime
+import boto3
+import botocore.exceptions
+
+from secrets import *
+
+
+def execute_vault_backup(cmd, workdir):
+    try:
+        c = subprocess.run([cmd], cwd=workdir)
+        if c.returncode == 0:
+            c = subprocess.run([r'tar', r'zcf', 'backups.tar.gz', 'Backups'], cwd=wd)
+            return c.returncode
+    except Exception as e:
+        return -1
+
+
+def upload_to_s3bucket(loc_file, s3_b, rem_file):
+    print(f"Uploading {loc_file} to s3://{s3_b}/{rem_file}...")
+
+    s3 = boto3.client('s3', aws_access_key_id=s3_access_key_id, aws_secret_access_key=s3_secret_access_key)
+
+    s3.upload_file(loc_file, s3_b, rem_file)
+
+
+bk_cmd = r'"C:\Program Files\Autodesk\Vault Server 2024\ADMS Console\Connectivity.ADMSConsole.exe" ' \
+         r'-Obackup -BC:\Backup\Backups -VUAdministrator  -VPCAD70_Inventor+ -S -LC:\Backup\BackupLog.txt'
+if len(sys.argv) > 1 and sys.argv[1] == "-d":
+    bk_cmd = os.path.join(wd, "dummybackup.bat")
+
+if execute_vault_backup(bk_cmd, wd) !=0:
+    print("Error executing vault backup command.")
+    exit(-1)
+
+n = datetime.datetime.now().strftime("%Y_%m_%d_%H_")
+tar_rem_name = f'vault_backup_{n}.tar.gz'
+tar_loc_fullname = os.path.join(wd, 'backups.tar.gz')
+
+try:
+    upload_to_s3bucket(tar_loc_fullname, s3_bucket, tar_rem_name)
+except (WindowsError, botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+    print('Error occurred.\n', e)
+    exit(-1)
+
+print("Uploaded to S3.")
+subprocess.run([r'cmd', r'/c', 'del', r'/Q', r'C:\backup\backups.tar.gz'])
